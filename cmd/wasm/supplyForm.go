2a37f67b8fab67e4db99ec8e2b4b5118d39bffc7
jb <1413880+werelord@users.noreply.github.com>
2025-06-27 16:04:35 -0500

    Update supplyForm.go

--- cmd/wasm/supplyForm.go
+++ cmd/wasm/supplyForm.go
@@ -25,8 +25,7 @@ func genSupplyForm(doc dom.Document, planetStr string) any {
 
 	title.SetInnerHTML(fmt.Sprintf("Planet \"%v\": Edit Supply", p.Name))
 	title.SetAttribute("data-planet", p.Name)
-	myShare.SetInnerHTML(fmt.Sprintf("My Share (total): %.2f%%",
-		float32(p.market.current)/float32(p.market.total)*100.0))
+	myShare.SetInnerHTML(fmt.Sprintf("My Share (total): %.2f%%", p.calcTotalShare()))
 
 	cat, share := p.calcMaxOppShare()
 	oppShare.SetInnerHTML(fmt.Sprintf("Max Opponent Share (%v): %.2f%%", cat.String(), share))
@@ -134,7 +133,7 @@ func genSupplyProductTable(doc dom.Document, p planetType) {
 	tbody.AppendChild(hidden)
 
 	spacer := doc.CreateElement("td")
-	spacer.SetInnerHTML("0000")
+	spacer.SetInnerHTML("000 / 000")
 
 	hidden.AppendChild(doc.CreateElement("td"))
 	hidden.AppendChild(doc.CreateElement("td"))
@@ -175,7 +174,7 @@ func genSupplyProductTable(doc dom.Document, p planetType) {
 
 			amt := doc.CreateElement("td")
 			amt.SetID(fmt.Sprintf("%v_supply", prod.ID()))
-			amt.SetInnerHTML(fmt.Sprintf("%v", prod.Supply))
+			amt.SetInnerHTML(fmt.Sprintf("%v / %v", prod.Supply, prod.Demand))
 
 			add := doc.CreateElement("td")
 			addButton := doc.CreateElement("button")
@@ -193,5 +192,109 @@ func genSupplyProductTable(doc dom.Document, p planetType) {
 
 func onChangeSupply(amt int, productStr string) any {
 
-	return sendErr("not yet implemented")
+	var (
+		doc       = dom.GetWindow().Document()
+		title     = doc.GetElementByID("supplyTitle")
+		planetStr = title.(*dom.HTMLDivElement).Dataset()["planet"]
+
+		planet  *planetType
+		product productType
+		// err error
+	)
+
+	fmt.Printf("onChangeSupply, %v:%v : %v\n", planetStr, productStr, amt)
+
+	if p, exists := planetMap[planetStr]; !exists {
+		return sendErr("Unable to find planet '%v' (something is wrong)", planetStr)
+	} else if prd, exists := p.ProductList[toId(productStr)]; !exists {
+		return sendErr("Planet '%v' does not have product '%v' (something is wrong)", planetStr, productStr)
+	} else {
+		planet = p
+		product = prd
+	}
+
+	if amt > 0 && product.Supply == product.Demand {
+		fmt.Printf("on add, max already reached; no op: %v: %v\n", planet.Name, planet.ProductList[productStr])
+		return nil
+	} else if amt < 0 && product.Supply == 0 {
+		fmt.Printf("on sub, min already reached; no op: %v: %v\n", planet.Name, planet.ProductList[productStr])
+		return nil
+	}
+
+	// check limits
+	var newAmt = planet.ProductList[product.ID()].Supply + amt
+	if newAmt < 0 {
+		newAmt = 0
+	} else if newAmt > product.Demand {
+		newAmt = product.Demand
+	}
+
+	fmt.Printf("before, %v:%v:%v:%v:%v\n", planet.Name, product.name,
+		planet.market, planet.marketByCat[product.category], planet.ProductList[productStr],
+	)
+
+	product.Supply = newAmt
+	planet.ProductList[product.ID()] = product
+
+	// adjust relative market volume
+
+	var newCurrent = amt * product.price
+	planet.market.current += newCurrent
+	var mktByCat = planet.marketByCat[product.category]
+	mktByCat.current += newCurrent
+	planet.marketByCat[product.category] = mktByCat // not stored by reference
+
+	fmt.Printf("after, %v:%v:%v:%v:%v\n", planet.Name, product.name,
+		planet.market, planet.marketByCat[product.category], planet.ProductList[product.ID()],
+	)
+
+	updatePlanetInfo(doc, *planet)
+	updateProductMarket(doc, *planet, product)
+	updateCategoryMarket(doc, *planet, product.category)
+	updateSupplyForm(doc, *planet, product)
+
+	return nil
+}
+
+func updatePlanetInfo(doc dom.Document, planet planetType) {
+
+	var newShare = float64(planet.market.current) / float64(planet.market.total) * 100
+
+	share := doc.GetElementByID(planet.Name).QuerySelector("#market_share")
+	share.SetInnerHTML(fmt.Sprintf("market share: %.2f%%", newShare))
+}
+
+func updateProductMarket(doc dom.Document, planet planetType, prod productType) {
+
+	prodDiv := doc.GetElementByID(planet.Name).QuerySelector("#" + prod.ID())
+	prodDiv.SetInnerHTML(fmt.Sprintf("%v:  %v / %v", prod.name, prod.Supply, prod.Demand))
+}
+
+func updateCategoryMarket(doc dom.Document, planet planetType, category categoryType) {
+	// just one category to update
+	categoryDiv := doc.GetElementByID(planet.Name).QuerySelector("#" + category.String())
+	cur, opp, _ := planet.calcCategoryShare(category)
+
+	categoryDiv.SetInnerHTML(fmt.Sprintf("%v: %.1f / %.1f%%", category.String(), cur, opp))
+}
+
+func updateSupplyForm(doc dom.Document, planet planetType, product productType) {
+
+	form := doc.GetElementByID("supplyForm")
+	myShare := form.QuerySelector("#infoMyShare")
+	myShare.SetInnerHTML(fmt.Sprintf("My Share (total): %.2f%%", planet.calcTotalShare()))
+
+	cat, share := planet.calcMaxOppShare()
+	oppShare := form.QuerySelector("#infoOppShare")
+	oppShare.SetInnerHTML(fmt.Sprintf("Max Opponent Share (%v): %.2f%%", cat.String(), share))
+
+	cur, opp, _ := planet.calcCategoryShare(product.category)
+	catDiv := form.QuerySelector("#sf_" + product.category.String())
+	curDiv := catDiv.NextElementSibling()
+	curDiv.SetInnerHTML(fmt.Sprintf("%.2f%%", cur))
+	oppDiv := curDiv.NextElementSibling()
+	oppDiv.SetInnerHTML(fmt.Sprintf("%.2f%%", opp))
+
+	amt := form.QuerySelector("#" + product.ID() + "_supply")
+	amt.SetInnerHTML(fmt.Sprintf("%v / %v", product.Supply, product.Demand))
 }
